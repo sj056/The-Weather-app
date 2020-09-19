89e80fd624603b0a96e67a95762ceaa09ca52483
Shruti Sharma <shrutijangid5601@gmail.com>
2020-09-20 00:38:35 +0530

    editted url

--- src/App.js
+++ src/App.js
@@ -74,7 +74,7 @@ class App extends React.Component{
     const country=e.target.elements.country.value;
     if(city&&country)
     {
-    const api_call=await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_key}`);
+    const api_call=await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_key}`);
 
     const response=await api_call.json();
     console.log(response);
